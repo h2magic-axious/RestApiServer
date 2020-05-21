5073815324520fe4251a93daaeeb5e5490a99e7c
h2magic <h2magic@126.com>
2020-05-21 11:28:40 +0800

    Create table

--- Trinamic/models.py
+++ Trinamic/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
+
 # Create your models here.
+class Product(models.Model):
+    id = models.AutoField(primary_key=True, auto_created=True)
+    name = models.CharField(max_length=50)
+    alias = models.CharField(max_length=50, default=None)
+    excerpt = models.TextField(default=None)
+
+    class Meta:
+        db_table = 'trinamic_product'
+
+
+class Category(models.Model):
+    id = models.AutoField(primary_key=True, auto_created=True)
+    name = models.CharField(max_length=50)
+    alias = models.CharField(max_length=50, default=None)
+    excerpt = models.TextField(default=None)
+
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'trinamic_category'
+
+
+class Item(models.Model):
+    id = models.BigAutoField(primary_key=True, auto_created=True)
+    model = models.CharField(max_length=50)
+    excerpt = models.TextField(default=None)
