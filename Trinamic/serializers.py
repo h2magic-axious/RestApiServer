c7330de0d401c34d87a3fdc194402f64748a6b9f
h2magic <h2magic@126.com>
2020-05-21 17:40:36 +0800

    Serizliae: Product, Category, Item

---
+++ Trinamic/serializers.py
@@ -0,0 +1,21 @@
+from rest_framework import serializers
+
+from Trinamic.models import Product, Category, Item
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['id', 'name', 'alias', 'excerpt', 'category_set']
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ['id', 'name', 'alias', 'excerpt', 'product_id', 'item_set']
+
+
+class ItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Item
+        fields = ['id', 'model', 'excerpt', 'category_id']
