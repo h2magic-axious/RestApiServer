69dcb188f1d9f0874c68a0edde8840842b792508
h2magic <h2magic@163.com>
2020-06-22 09:59:12 +0800

    The same as last commit

--- Trinamic/urls.py
+++ Trinamic/urls.py
@@ -9,9 +9,9 @@ urlpatterns = [
     path('category', views.CategoryList.as_view()),
     path('category/<int:pk>', views.CategoryDetail.as_view()),
     path('category/whole', views.whole_categories),
-    path('category/whole/<int:p_id>'),
+    path('category/whole/<int:p_id>', views.whole_categories_with_product),
     path('item', views.ItemList.as_view()),
     path('item/<int:pk>', views.ItemDetail.as_view()),
     path('item/whole', views.whole_items),
-    path('item/whole/<int:c_id>'),
+    path('item/whole/<int:c_id>', views.whole_items_with_category),
 ]
