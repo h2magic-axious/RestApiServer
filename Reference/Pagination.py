b00c1e379f71b0eca1f33575ba67d63d4e7fbe25
h2magic <h2magic@163.com>
2020-06-28 09:14:38 +0800

    change the page size to 7

--- Reference/Pagination.py
+++ Reference/Pagination.py
@@ -2,7 +2,7 @@ from rest_framework.pagination import PageNumberPagination
 
 
 class OwnPagination(PageNumberPagination):
-    page_size = 30
+    page_size = 7
     page_size_query_param = 'size'
     page_query_param = 'page'
     max_page_size = None
