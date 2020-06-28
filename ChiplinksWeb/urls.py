d790da019db53c55c801b6a12be03fb69ef7203d
h2magic <h2magic@163.com>
2020-06-28 11:51:04 +0800

    reset api server

--- ChiplinksWeb/urls.py
+++ ChiplinksWeb/urls.py
@@ -29,5 +29,5 @@ urlpatterns = [
     path(RESTFUL_URL, include('Media.urls')),
     path(RESTFUL_URL, include('Resource.urls')),
     path(RESTFUL_URL, include('Blog.urls')),
-    path('api/search/<str:query>', search)
+    path('api/private/v1/search/<str:query>', search)
 ]
