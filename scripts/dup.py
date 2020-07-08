b4a08782ffa229fad123cb59e9e85e22d58c299c
h2magic <h2magic@163.com>
2020-07-08 16:25:27 +0800

    update data

--- scripts/dup.py
+++ scripts/dup.py
@@ -2,6 +2,35 @@ import os, sys
 import django
 import json
 
+from pathlib import Path
+
+from googletrans import Translator
+
+QINIU_DNS_NAME = 'http://tmc-item.chiplinkstech.com/'
+
+
+def format_url(url: str):
+    if url is None:
+        return url
+
+    filename = url.split('/')[-1]
+    return QINIU_DNS_NAME + filename
+
+
+class GTranslator:
+    def __init__(self, host='translate.google.cn', dest='zh-CN'):
+        self.__tr = Translator(service_urls=[host])
+        self.__dest = dest
+
+    def translate(self, text):
+        try:
+            print("Translating: ", text)
+            return self.__tr.translate(text, self.__dest).text
+        except:
+            print("Translate failure")
+            return text
+
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 sys.path.append(BASE_DIR)
@@ -15,28 +44,40 @@ from Media.models import Media, MEDIA_TYPES, MEDIA_TAGS
 from Resource.models import ResourceType, ResourceContent
 from Blog.models import BlogCategory, BlogTag, BlogArticle, BlogComment
 
-with open(r'Demo\TMC262-LA.json', 'r', encoding='utf-8') as f:
-    data = json.load(f)
+pro = Path(r'E:\workspace\Trinamic\Memory\Trinamic_Json\map.json')
 
-obj = Item.objects.get(model=data['model'])
-obj.excerpt = data['excerpt']
+translator = GTranslator()
 
-for media in data['media']:
-    Media.objects.create(title=media['name'],
-                         tag=media['tag'],
-                         media_type=media['type'],
-                         using_url=media['url'],
-                         item=obj)
+field_history = {field.name: field for field in Field.objects.all()}
+resource_history = {res.name: res for res in ResourceType.objects.all()}
 
-for field in data['fields']:
-    f, v = field
-    f_obj = Field.objects.get(name=f)
-    FieldValue.objects.create(field=f_obj, value=v, item=obj)
+items = {item.model: item for item in Item.objects.all()}
 
-for rt, rcs in data['resource'].items():
-    rt_obj = ResourceType.objects.get(name=rt)
-    for name, url in rcs:
-        ResourceContent.objects.create(resource_type=rt_obj,
-                                       name=name,
-                                       using_url=url,
-                                       item=obj)
+
+# def get_field(name):
+#     if name in field_history:
+#         return field_history[name]
+#     else:
+#         field = Field.objects.create(name=name, alias=name)
+#         field_history[name] = field
+#         return field
+#
+#
+# def get_resource_type(name):
+#     if name in resource_history:
+#         return resource_history[name]
+#     else:
+#         res_type = ResourceType.objects.create(name=name, alias=name)
+#         resource_history[name] = res_type
+#         return res_type
+#
+#
+# model_list = {item.model: item for item in Item.objects.all()}
+#
+# with open(pro, 'r', encoding='utf-8') as f_painter:
+#     data_list = json.load(f_painter)
+#
+# for model, detail in data_list.items():
+#     item = model_list[model]
+#     item.excerpt = translator.translate(detail['excerpt'])
+#     item.save()
